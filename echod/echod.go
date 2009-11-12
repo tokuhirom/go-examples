778407a37b78bdec5c501f37f30e70bb18f60ed5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-11-12 18:59:04 +0900

    initial import

---
+++ echod/echod.go
@@ -0,0 +1,45 @@
+package main
+import (
+    "net";
+    "os";
+    "fmt";
+)
+
+func Err(format string, v ...) {
+    fmt.Fprintf(os.Stderr, format + "\n", v)
+}
+
+func Handler(conn net.Conn) {
+    buffer := make([]byte, 24);
+    for {
+        l, e := conn.Read(buffer);
+        switch {
+        case e == nil:
+            conn.Write(buffer[0:l])
+        case e == os.EOF:
+            break;
+        case e != os.EAGAIN:
+            Err("Err on receiving a header (%s)", e);
+            break;
+        }
+    }
+    conn.Close();
+}
+
+func main() {
+    psock, e := net.Listen("tcp", "127.0.0.1:1978");
+    if e != nil {
+        Err("an error occured(%s)", e);
+        return
+    }
+
+    for {
+        conn, e := psock.Accept();
+        if e != nil {
+            Err("an error occured(%s)", e);
+            return
+        }
+        go Handler(conn);
+    }
+}
+

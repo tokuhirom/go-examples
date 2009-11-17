928feada958321beb4040d1332a56dc7af6bccef
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-11-17 09:35:10 +0900

    simplified

--- bbs/bbs.go
+++ bbs/bbs.go
@@ -11,13 +11,7 @@ import (
     "os";
 );
 
-var addr = flag.String("addr", "0.0.0.0:1718", "http service address") // Q=17, R=18
-var fmap = template.FormatterMap{
-    "html":     template.HTMLFormatter,
-    "url+html": UrlHtmlFormatter,
-}
-var templ = template.MustParse(templateStr, fmap)
-var storage = new(vector.StringVector);
+var addr = flag.String("addr", "0.0.0.0:1718", "http service address")
 
 type tmplParams struct {
     msgs string;
@@ -39,8 +33,15 @@ func html_escape(src string) string {
 
 func main() {
     flag.Parse();
+    templ := func () *template.Template {
+        templateStr, err := io.ReadFile("tmpl/top.tmpl");
+        if err != nil {
+            log.Exit(err);
+        }
+        return template.MustParse(string(templateStr), nil);
+    }();
+    storage := new(vector.StringVector);
 
-    fmt.Printf("http://%s/\n", *addr);
     http.Handle("/", http.HandlerFunc(func(c *http.Conn, req *http.Request) {
         params := new(tmplParams);
         part := "<ul>\n";
@@ -64,45 +65,11 @@ func main() {
     http.Handle("/css/", http.HandlerFunc(func(c *http.Conn, req *http.Request) {
         http.ServeFile(c, req, "." + req.URL.Path);
     }));
+
+    fmt.Printf("http://%s/\n", *addr);
     err := http.ListenAndServe(*addr, nil);
     if err != nil {
         log.Exit("ListenAndServe:", err);
     }
 }
-
-func UrlHtmlFormatter(w io.Writer, v interface{}, fmt string) {
-    template.HTMLEscape(w, strings.Bytes(http.URLEscape(v.(string))));
-}
-
-const templateStr = `
-<html>
-<head>
-    <title>bbs</title>
-    <link rel="stylesheet" href="/css/blueprint/screen.css" type="text/css" media="screen, projection">
-    <link rel="stylesheet" href="/css/blueprint/print.css" type="text/css" media="print">
-    <!--[if lt IE 8]><link rel="stylesheet" href="/css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
-    <link rel="stylesheet" href="/css/bbs.css" type="text/css" media="screen, projection">
-</head>
-<body>
-    <div class="container">
-        <div class="span-24 last header">
-            go bbs
-        </div>
-        <div class="span-24 last">
-            <form method="post" action="/post">
-                <input type="text" name="body" value="" />
-                <input type="submit" value="post" />
-            </form>
-        </div>
-        <div class="span-24 last">
-            {msgs}
-        </div>
-        <hr />
-        <div class="span-24 last footer">
-            powered by <a href="http://golang.org/">go</a>.
-        </div>
-    </div>
-</body>
-</html>
-`;
 

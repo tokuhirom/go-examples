cfd2e65ec8c0b0e1e9d394c91a99abea4a52eabb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-11-18 14:12:02 +0900

    use sqlite3

--- bbs/bbs.go
+++ bbs/bbs.go
@@ -7,11 +7,29 @@ import (
     "flag";
     "log";
     "container/vector";
+    "sqlite3";
 );
 
 var addr = flag.String("addr", "0.0.0.0:1718", "http service address")
 
 func main() {
+    sqlite3.Initialize();
+    defer func () {
+        log.Stdout("closing sqlite3");
+        sqlite3.Shutdown();
+    }();
+
+    dbh := new(sqlite3.Handle);
+    dbh.Open("bbs.db");
+    defer dbh.Close();
+
+    st,err := dbh.Prepare("CREATE TABLE IF NOT EXISTS entry (id INTEGER PRIMARY KEY, body VARCHAR(255));");
+    if (err != "") {
+        log.Exit(err);
+    }
+    st.Step();
+    st.Finalize();
+
     flag.Parse();
     templ := func () *template.Template {
         templateStr, err := io.ReadFile("tmpl/top.tmpl");
@@ -20,10 +38,30 @@ func main() {
         }
         return template.MustParse(string(templateStr), nil);
     }();
-    storage := new(vector.StringVector);
 
     http.Handle("/", http.HandlerFunc(func(c *http.Conn, req *http.Request) {
         params := new(struct { msgs []string });
+        st,err = dbh.Prepare("SELECT * from entry ORDER BY id DESC limit 30;");
+        if err != "" {
+            log.Exit(err);
+        }
+        storage := new(vector.StringVector);
+        func () {
+            for {
+                rv := st.Step();
+                switch {
+                case rv==sqlite3.SQLITE_DONE:
+                    return;
+                case rv==sqlite3.SQLITE_ROW:
+                    body := st.ColumnText(1);
+                    storage.Push(body);
+                default:
+                    println(rv);
+                    log.Exit(dbh.ErrMsg());
+                }
+            };
+        }();
+        st.Finalize();
         params.msgs = storage.Data();
         err := templ.Execute(params, c);
         if err != nil {
@@ -32,18 +70,31 @@ func main() {
     }));
     http.Handle("/post", http.HandlerFunc(func(c *http.Conn, req *http.Request) {
         req.ParseForm();
+
         body := req.Form["body"][0];
-        storage.Insert(0, body);
+        st,err = dbh.Prepare("INSERT INTO entry (body) VALUES (?)");
+        if err != "" {
+            log.Exit(err);
+        }
+        if st.BindText(1, body) != sqlite3.SQLITE_OK {
+            log.Exit("cannot bind: ", dbh.ErrMsg());
+        }
+        st.Step();
+        st.Finalize();
+
         http.Redirect(c, "/", 302);
     }));
     http.Handle("/css/", http.HandlerFunc(func(c *http.Conn, req *http.Request) {
         http.ServeFile(c, req, "." + req.URL.Path);
     }));
 
-    fmt.Printf("http://%s/\n", *addr);
-    err := http.ListenAndServe(*addr, nil);
-    if err != nil {
-        log.Exit("ListenAndServe:", err);
-    }
+    // run httpd
+    func() {
+        fmt.Printf("http://%s/\n", *addr);
+        err := http.ListenAndServe(*addr, nil);
+        if err != nil {
+            log.Exit("ListenAndServe:", err);
+        }
+    }();
 }
 
